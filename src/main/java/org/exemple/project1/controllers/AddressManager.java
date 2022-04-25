c86db46b13c5a67612e50826abb3d5c202119c50
toxigit <hamdiharzallah25@gmail.com>
2022-04-25 23:18:39 +0100

    address and city manager controllers

--- src/main/java/org/exemple/project1/controllers/AddressManager.java
+++ src/main/java/org/exemple/project1/controllers/AddressManager.java
@@ -1,4 +1,19 @@
 package org.exemple.project1.controllers;
 
-public class AddressManager {
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.exemple.project1.entities.Address;
+
+public class AddressManager implements GenericDAO<Address,Integer> {
+    @Inject
+    private EntityManager manager;
+    @Override
+    public EntityManager getEntityManager() {
+        return manager;
+    }
+
+    @Override
+    public Class<Address> getEntityClass() {
+        return Address.class;
+    }
 }
--- src/main/java/org/exemple/project1/controllers/CityManager.java
+++ src/main/java/org/exemple/project1/controllers/CityManager.java
@@ -1,4 +1,19 @@
 package org.exemple.project1.controllers;
 
-public class CityManager {
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.exemple.project1.entities.City;
+
+public class CityManager implements GenericDAO<City,Integer> {
+    @Inject
+    private EntityManager manager;
+    @Override
+    public EntityManager getEntityManager() {
+        return manager;
+    }
+
+    @Override
+    public Class<City> getEntityClass() {
+        return City.class;
+    }
 }
